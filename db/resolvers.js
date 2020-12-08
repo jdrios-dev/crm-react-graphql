e6969332f434a19b738206fdb547aa5024ef63c0
Juan Daniel Rios Ramirez <mrdaniel19996@gmail.com>
2020-12-08 08:25:50 -0500

    [UPDATE]Auth with JWT

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,10 +1,22 @@
 const Usuario = require('../models/Usuario');
-const bcryptjs = require('bcryptjs')
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: '.env' });
+
+const crearToken = (usuario, secreta, expiresIn) => {
+  const { id, email, nombre, apelido } = usuario;
+
+  return jwt.sign({ id, email, nombre, apelido  }, secreta, { expiresIn })
+}
 
 //Resolvers
 const resolvers = {
   Query: {
-    obtenerCurso: () => 'Algo'
+    obtenerUsuario: async (_, { token }) => {
+      const usuarioId = await jwt.verify(token, process.env.SECRETA)
+
+      return usuarioId;
+    },
   },
   Mutation: {
     nuevoUsuario: async (_, { input }) => {
@@ -27,6 +39,28 @@ const resolvers = {
       } catch (error) {
         console.log(error);
       }
+    },
+
+    autenticarUsuario: async (_, { input }) => {
+      const { email, password } = input;
+      //Revisar si existe
+      const existeUsuario = await Usuario.findOne({ email });
+      if (!existeUsuario) {
+        throw new Error('El usuario no esta registrado');
+      }
+
+      //Revisa password
+      const passwordCorrecto = await bcryptjs.compare( password, existeUsuario.password );
+      if( !passwordCorrecto ) {
+        throw new Error('El password es incorrecto');
+      }
+
+      //Crear Token
+
+      return {
+        token: crearToken(existeUsuario, process.env.SECRETA, '24h')
+      }
+
     }
   }
 }
