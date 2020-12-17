89e9ccc8f432c211ec2071db9d99cac7de223590
Juan Daniel Rios Ramirez <mrdaniel19996@gmail.com>
2020-12-16 22:18:56 -0500

    [UPDATE] resolver nuevo usario

--- db/resolvers.js
+++ db/resolvers.js
@@ -15,10 +15,8 @@ const crearToken = (usuario, secreta, expiresIn) => {
 //Resolvers
 const resolvers = {
   Query: {
-    obtenerUsuario: async (_, { token }) => {
-      const usuarioId = await jwt.verify(token, process.env.SECRETA)
-
-      return usuarioId;
+    obtenerUsuario: async (_, {  }, ctx) => {
+      return ctx.usuario;
     },
     obtenerProductos: async () => {
       try {
